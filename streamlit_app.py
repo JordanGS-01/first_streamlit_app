a0dbf858bb62e23e58a08c705e47ea93f49be8a3
Jordán GS <136735844+JordanGS-01@users.noreply.github.com>
2023-08-29 08:26:02 -0600

    Update streamlit_app.py
    Added an input so the user can search information about the fruits he wants

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,11 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choise = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choise)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
 
 # Normalize the JSON data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
