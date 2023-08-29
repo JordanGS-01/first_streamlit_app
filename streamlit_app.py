fe60a1fd9cc10956f3c7fb6046343e49ed8a8ac4
Jordán GS <136735844+JordanGS-01@users.noreply.github.com>
2023-08-29 08:20:39 -0600

    Update streamlit_app.py
    Normalize the API response and renders it as a dataframe into the GUI

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,9 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # Just writes the data to the screen
+
+# Normalize the JSON data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Displays the API response to the screen as a pandas dataframe
+streamlit.dataframe(fruityvice_normalized)
